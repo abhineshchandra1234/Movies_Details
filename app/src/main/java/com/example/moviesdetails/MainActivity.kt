93817658e315855c834d2142ec9ecfe9ad7da3ee
abhineshchandra1234 <abhineshchandra1234@gmail.com>
2023-12-31 11:31:17 +0530

    movie list is shown on the screen

--- app/src/main/java/com/example/moviesdetails/MainActivity.kt
+++ app/src/main/java/com/example/moviesdetails/MainActivity.kt
@@ -4,9 +4,24 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
@@ -14,8 +29,11 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.moviesdetails.ui.theme.MoviesDetailsTheme
 
 class MainActivity : ComponentActivity() {
@@ -51,12 +69,65 @@ fun MyApp(content: @Composable (PaddingValues) -> Unit) {
 }
 
 @Composable
-fun MainContent(paddingValues: PaddingValues) {
-    Surface(
-        modifier = Modifier.padding(top = paddingValues.calculateTopPadding()),
-        color = MaterialTheme.colorScheme.background
+fun MainContent(
+    paddingValues: PaddingValues, movieList: List<String> =
+        listOf(
+            "Avatar",
+            "300",
+            "Harry Potter",
+            "Life",
+            "Hapiness",
+            "Cross the line",
+            "The Shawshank Redemption",
+            "The Godfather",
+            "The Dark Knight",
+            "The Godfather Part II",
+            "12 Angry Men"
+        )
+) {
+    Surface(modifier = Modifier.padding(top = paddingValues.calculateTopPadding())) {
+        Column(modifier = Modifier.padding(12.dp)) {
+            LazyColumn() {
+                items(items = movieList) {
+                    MovieRow(movie = it)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun MovieRow(movie: String) {
+    Card(
+        modifier = Modifier
+            .padding(4.dp)
+            .fillMaxWidth()
+            .height(130.dp),
+        shape = RoundedCornerShape(
+            corner = CornerSize(16.dp)
+        ),
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 6.dp
+        )
     ) {
-        Text(text = "Hello")
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Start
+        ) {
+            Surface(
+                modifier = Modifier
+                    .padding(12.dp)
+                    .size(100.dp),
+                shape = RectangleShape,
+                shadowElevation = 4.dp
+            ) {
+                Icon(
+                    imageVector = Icons.Default.AccountBox,
+                    contentDescription = "Movie Image"
+                )
+            }
+            Text(text = movie)
+        }
     }
 }
 
