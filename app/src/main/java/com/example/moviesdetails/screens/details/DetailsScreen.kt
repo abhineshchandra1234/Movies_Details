0eaa50c3da4894358d312648382f93cf9b0d0f37
abhineshchandra1234 <abhineshchandra1234@gmail.com>
2024-01-04 12:26:02 +0530

    added back btn navigation for details screen

--- app/src/main/java/com/example/moviesdetails/screens/details/DetailsScreen.kt
+++ app/src/main/java/com/example/moviesdetails/screens/details/DetailsScreen.kt
@@ -2,14 +2,18 @@ package com.example.moviesdetails.screens.details
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 
 
@@ -28,7 +32,13 @@ fun DetailsScreen(
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Center
         ) {
-            Text(text = movieData.toString(), style = MaterialTheme.typography.bodyMedium)
+            Text(text = movieData.toString(), style = MaterialTheme.typography.headlineMedium)
+            Spacer(modifier = Modifier.height(23.dp))
+            Button(onClick = {
+                navController.popBackStack()
+            }) {
+                Text(text = "Go Back")
+            }
         }
     }
 }
