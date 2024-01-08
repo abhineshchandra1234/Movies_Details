9b282b6c6597a894b128e5613031fed6f4864f94
abhineshchandra1234 <abhineshchandra1234@gmail.com>
2024-01-08 11:58:22 +0530

    fetched images from internet using coil

--- app/src/main/java/com/example/moviesdetails/widgets/widgets.kt
+++ app/src/main/java/com/example/moviesdetails/widgets/widgets.kt
@@ -1,7 +1,9 @@
 package com.example.moviesdetails.widgets
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -9,23 +11,27 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AccountBox
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.compose.rememberImagePainter
+import coil.transform.CircleCropTransformation
 import com.example.moviesdetails.model.Movie
+import com.example.moviesdetails.model.getMovies
 
+
+@Preview
 @Composable
 fun MovieRow(
-    movie: Movie,
+    movie: Movie = getMovies()[0],
     onItemClick: (String) -> Unit = {}
 ) {
     Card(
@@ -54,12 +60,34 @@ fun MovieRow(
                 shape = RectangleShape,
                 shadowElevation = 4.dp
             ) {
-                Icon(
-                    imageVector = Icons.Default.AccountBox,
-                    contentDescription = "Movie Image"
+//                Icon(
+//                    imageVector = Icons.Default.AccountBox,
+//                    contentDescription = "Movie Image"
+//                )
+                Image(
+                    painter = rememberImagePainter(data = movie.images[0],
+                        builder = {
+                            crossfade(true)
+                            transformations(CircleCropTransformation())
+                        }),
+                    contentDescription = "Movie Poster"
                 )
             }
-            Text(text = movie.title)
+            Column(modifier = Modifier.padding(4.dp)) {
+
+                Text(
+                    text = movie.title,
+                    style = MaterialTheme.typography.headlineSmall
+                )
+                Text(
+                    text = "Director: ${movie.director}",
+                    style = MaterialTheme.typography.bodySmall
+                )
+                Text(
+                    text = "Released: ${movie.year}",
+                    style = MaterialTheme.typography.bodySmall
+                )
+            }
         }
     }
 }
