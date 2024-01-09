a00d43e2a9206123cc1b50720409355287f38574
abhineshchandra1234 <abhineshchandra1234@gmail.com>
2024-01-09 13:11:27 +0530

    added drop down animation for text

--- app/src/main/java/com/example/moviesdetails/widgets/widgets.kt
+++ app/src/main/java/com/example/moviesdetails/widgets/widgets.kt
@@ -1,5 +1,6 @@
 package com.example.moviesdetails.widgets
 
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -11,14 +12,22 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
@@ -37,6 +46,9 @@ fun MovieRow(
     movie: Movie = getMovies()[0],
     onItemClick: (String) -> Unit = {}
 ) {
+    var expanded by remember {
+        mutableStateOf(false)
+    }
     Card(
         modifier = Modifier
             .padding(4.dp)
@@ -89,6 +101,24 @@ fun MovieRow(
                 Text(
                     text = "Released: ${movie.year}",
                     style = MaterialTheme.typography.bodySmall
+                )
+
+                AnimatedVisibility(visible = expanded) {
+
+                    Column {
+                        Text(text = "Hello There")
+                    }
+                }
+
+                Icon(
+                    imageVector = Icons.Filled.KeyboardArrowDown,
+                    contentDescription = "Down Arrow",
+                    modifier = Modifier
+                        .size(25.dp)
+                        .clickable {
+                            expanded = !expanded
+                        },
+                    tint = Color.DarkGray
                 )
             }
         }
