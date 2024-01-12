6be1b8f72c31e63bf4db2aab06f33b422874f6b8
abhineshchandra1234 <abhineshchandra1234@gmail.com>
2024-01-12 11:59:00 +0530

    added images for each movies in horizontal list

--- app/src/main/java/com/example/moviesdetails/screens/details/DetailsScreen.kt
+++ app/src/main/java/com/example/moviesdetails/screens/details/DetailsScreen.kt
@@ -1,5 +1,6 @@
 package com.example.moviesdetails.screens.details
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -7,10 +8,17 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -22,40 +30,43 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import coil.compose.rememberAsyncImagePainter
+import coil.request.ImageRequest
+import com.example.moviesdetails.model.Movie
+import com.example.moviesdetails.model.getMovies
+import com.example.moviesdetails.widgets.MovieRow
 
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DetailsScreen(
-    navController: NavController,
-    movieData: String?
+    navController: NavController, movieId: String?
 ) {
-
-    Scaffold(
-        topBar = {
-            TopAppBar(
-                title = {
-                    Row(horizontalArrangement = Arrangement.Start) {
-                        Icon(
-                            imageVector = Icons.Default.ArrowBack,
-                            contentDescription = "Arrow Back",
-                            modifier = Modifier.clickable {
-                                navController.popBackStack()
-                            }
-                        )
-                        Spacer(modifier = Modifier.width(100.dp))
-                        Text(text = "Movies")
-                    }
-                },
-                colors = TopAppBarDefaults.topAppBarColors(
-                    containerColor = MaterialTheme.colorScheme.onPrimaryContainer,
-                    titleContentColor = MaterialTheme.colorScheme.primary
-                )
+    val newMovieList = getMovies().filter { movie ->
+        movie.id == movieId
+    }
+    Scaffold(topBar = {
+        TopAppBar(
+            title = {
+                Row(horizontalArrangement = Arrangement.Start) {
+                    Icon(imageVector = Icons.Default.ArrowBack,
+                        contentDescription = "Arrow Back",
+                        modifier = Modifier.clickable {
+                            navController.popBackStack()
+                        })
+                    Spacer(modifier = Modifier.width(100.dp))
+                    Text(text = "Movies")
+                }
+            }, colors = TopAppBarDefaults.topAppBarColors(
+                containerColor = MaterialTheme.colorScheme.onPrimaryContainer,
+                titleContentColor = MaterialTheme.colorScheme.primary
             )
-        }
-    ) {
+        )
+    }) {
         Surface(
             modifier = Modifier
                 .fillMaxHeight()
@@ -64,9 +75,39 @@ fun DetailsScreen(
         ) {
             Column(
                 horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.Center
+                verticalArrangement = Arrangement.Top
             ) {
-                Text(text = movieData.toString(), style = MaterialTheme.typography.headlineMedium)
+                MovieRow(movie = newMovieList.first())
+                Spacer(modifier = Modifier.height(8.dp))
+                Divider()
+                Text(text = "Movie Images")
+                HorizontalScrollableImageView(newMovieList)
+            }
+        }
+    }
+}
+
+@Composable
+private fun HorizontalScrollableImageView(newMovieList: List<Movie>) {
+    LazyRow {
+        items(newMovieList[0].images) { image ->
+            Card(
+                modifier = Modifier
+                    .padding(12.dp)
+                    .size(240.dp),
+                elevation = CardDefaults.cardElevation(
+                    defaultElevation = 5.dp
+                )
+            ) {
+                Image(
+                    painter = rememberAsyncImagePainter(
+                        model = ImageRequest.Builder(LocalContext.current)
+                            .data(data = image)
+                            .build()
+                    ),
+                    contentDescription = "Movie Poster",
+                    contentScale = ContentScale.Crop
+                )
             }
         }
     }
